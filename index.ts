8227b7b41968b3e32bc1971a6393b2a2674bb613
Federico Agnello <78120567+FedeDC512@users.noreply.github.com>
2025-05-08 22:24:55 +0200

    Initial commit

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import * as XLSX from "xlsx";
+
+// Legge il file Excel
+const workbook = XLSX.readFile("input.xlsx");
+
+// Il primo foglio contiene i dati
+const sheetName = workbook.SheetNames[0];
+const sheet = workbook.Sheets[sheetName];
+
+// Conversione in array di oggetti
+const data = XLSX.utils.sheet_to_json(sheet);
+
+console.log("Carte trovate nel file Excel:");
+console.log(data);
